542cc4ee2af9b4862ba905626151d9e9a3cfbf70
oct8l <gitmail@oct8l.email>
2023-04-20 19:24:40 -0500

    Initial commit

---
+++ RiightMedia.lua
@@ -0,0 +1,33 @@
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_BORDER = LSM.MediaType.BORDER
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cffcc8800Heal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Heal.ogg]])
+LSM:Register("sound", "|cffcc8800Frontal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Frontal.ogg]])
+LSM:Register("sound", "|cffcc8800Sexy Worms - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Sexy Worms.ogg]])
+LSM:Register("sound", "|cffcc8800AHH OHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AHH OHH.ogg]])
+LSM:Register("sound", "|cffcc8800AAAHHHHHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AAAHHHHHH.ogg]])
+LSM:Register("sound", "|cffcc8800CC - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - CC.ogg]])
+LSM:Register("sound", "|cffcc8800Jump - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Jump.ogg]])
+LSM:Register("sound", "|cffcc8800In - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - In.ogg]])
+LSM:Register("sound", "|cffcc8800Out - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Out.ogg]])
