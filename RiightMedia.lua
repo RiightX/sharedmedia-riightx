b563f897a1145306fe664f37455fd2de068ca0d4
RiightX <82491290+RiightX@users.noreply.github.com>
2024-01-22 16:45:08 -0600

    Some work (#1)
    * Add wee audio for a dumb weakaura
    
    * Remove some language stuff
    
    * Change font file and add license
    
    * Remove a sound
    
    * Remove a couple unused sounds

--- RiightMedia.lua
+++ RiightMedia.lua
@@ -1,6 +1,5 @@
 
 local LSM = LibStub("LibSharedMedia-3.0")
-local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
 
 local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
 local MediaType_BORDER = LSM.MediaType.BORDER
@@ -24,7 +23,6 @@ local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 -- -----
 LSM:Register("sound", "|cffcc8800Heal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Heal.ogg]])
 LSM:Register("sound", "|cffcc8800Frontal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Frontal.ogg]])
-LSM:Register("sound", "|cffcc8800Sexy Worms - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Sexy Worms.ogg]])
 LSM:Register("sound", "|cffcc8800AHH OHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AHH OHH.ogg]])
 LSM:Register("sound", "|cffcc8800AAAHHHHHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AAAHHHHHH.ogg]])
 LSM:Register("sound", "|cffcc8800CC - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - CC.ogg]])
@@ -32,3 +30,4 @@ LSM:Register("sound", "|cffcc8800Jump - Riight|r", [[Interface\Addons\SharedMedi
 LSM:Register("sound", "|cffcc8800In - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - In.ogg]])
 LSM:Register("sound", "|cffcc8800Out - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Out.ogg]])
 LSM:Register("sound", "|cffcc8800Personal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Personal.ogg]])
+LSM:Register("sound", "|cffcc8800Wee - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Wee.ogg]])
