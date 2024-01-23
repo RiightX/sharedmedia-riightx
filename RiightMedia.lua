a4315e8919f4a9a50487eca9c93011e4f9111297
RiightX <riightwow@gmail.com>
2024-01-22 18:22:27 -0600

    Fix some spacing

--- RiightMedia.lua
+++ RiightMedia.lua
@@ -21,13 +21,13 @@ local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "|cffcc8800Heal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Heal.ogg]])
-LSM:Register("sound", "|cffcc8800Frontal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Frontal.ogg]])
-LSM:Register("sound", "|cffcc8800AHH OHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AHH OHH.ogg]])
-LSM:Register("sound", "|cffcc8800AAAHHHHHH - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - AAAHHHHHH.ogg]])
-LSM:Register("sound", "|cffcc8800CC - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - CC.ogg]])
-LSM:Register("sound", "|cffcc8800Jump - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Jump.ogg]])
-LSM:Register("sound", "|cffcc8800In - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - In.ogg]])
-LSM:Register("sound", "|cffcc8800Out - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Out.ogg]])
-LSM:Register("sound", "|cffcc8800Personal - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Personal.ogg]])
-LSM:Register("sound", "|cffcc8800Wee - Riight|r", [[Interface\Addons\SharedMedia_Riight\sound\Riight - Wee.ogg]])
+LSM:Register("sound", "|cffcc8800Heal - Riight|r",          [[Interface\Addons\SharedMedia_Riight\sound\Riight - Heal.ogg]])
+LSM:Register("sound", "|cffcc8800Frontal - Riight|r",       [[Interface\Addons\SharedMedia_Riight\sound\Riight - Frontal.ogg]])
+LSM:Register("sound", "|cffcc8800AHH OHH - Riight|r",       [[Interface\Addons\SharedMedia_Riight\sound\Riight - AHH OHH.ogg]])
+LSM:Register("sound", "|cffcc8800AAAHHHHHH - Riight|r",     [[Interface\Addons\SharedMedia_Riight\sound\Riight - AAAHHHHHH.ogg]])
+LSM:Register("sound", "|cffcc8800CC - Riight|r",            [[Interface\Addons\SharedMedia_Riight\sound\Riight - CC.ogg]])
+LSM:Register("sound", "|cffcc8800Jump - Riight|r",          [[Interface\Addons\SharedMedia_Riight\sound\Riight - Jump.ogg]])
+LSM:Register("sound", "|cffcc8800In - Riight|r",            [[Interface\Addons\SharedMedia_Riight\sound\Riight - In.ogg]])
+LSM:Register("sound", "|cffcc8800Out - Riight|r",           [[Interface\Addons\SharedMedia_Riight\sound\Riight - Out.ogg]])
+LSM:Register("sound", "|cffcc8800Personal - Riight|r",      [[Interface\Addons\SharedMedia_Riight\sound\Riight - Personal.ogg]])
+LSM:Register("sound", "|cffcc8800Wee - Riight|r",           [[Interface\Addons\SharedMedia_Riight\sound\Riight - Wee.ogg]])
